0f54164f16264ed1ea87b0ee4476a4851f55132b
visavi <visavi.net@mail.ru>
2018-09-19 13:57:24 +0300

    first commit

---
+++ src/Module/Phpixie.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Codeception\Module;
+
+use Codeception\Lib\Framework;
+use Codeception\Lib\Interfaces\ActiveRecord;
+use Codeception\Lib\Interfaces\PartedModule;
+
+class Phpixie extends Framework implements ActiveRecord, PartedModule
+{
+    public function _initialize()
+    {
+
+    }
+
+    public function haveRecord($model, $attributes = [])
+    {
+
+    }
+
+    public function seeRecord($model, $attributes = [])
+    {
+
+    }
+
+    public function dontSeeRecord($model, $attributes = [])
+    {
+
+    }
+
+    public function grabRecord($model, $attributes = [])
+    {
+
+    }
+
+    public function _parts()
+    {
+
+    }
+}
